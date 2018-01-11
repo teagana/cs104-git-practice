4b16afde4539e1866b151efc80411716fd85cc76
Teagan Ampe <ampe@usc.edu>
2018-01-10 16:22:50 -0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,8 @@
 int main() 
 {
   std::cout << "HelloWorld!" << std::endl;
-  std::cout << "cs 104 is a good class" << endl;
+  std::cout << "cs 104 is a good class" << std::endl;
+  std::cout << "this is a change" << std::endl;
 
   return 0;
 }
